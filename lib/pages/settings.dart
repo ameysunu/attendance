71d2a948ec0cf0b54827ddf76f8dd255d4a6f8e4
Amey Sunu <ameysunu00719@gmail.com>
2020-10-23 10:57:01 +0300

    settings updated

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -13,9 +13,10 @@ class _SettingsState extends State<Settings> {
     return Scaffold(
       backgroundColor: Colors.blueGrey[50],
       appBar: AppBar(
+        centerTitle: true,
         elevation: 0,
         automaticallyImplyLeading: false,
-        backgroundColor: Colors.blueGrey[50],
+        backgroundColor: Colors.white,
         title: Text(
           "設定",
           style: TextStyle(color: Colors.black),
@@ -23,28 +24,77 @@ class _SettingsState extends State<Settings> {
       ),
       body: Column(
         children: [
-          Center(
-            child: RaisedButton(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              color: Colors.blueGrey[900],
-              textColor: Colors.white,
-              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 120),
-              onPressed: () {
-                signOut();
-                Navigator.pushReplacement(
-                    context, MaterialPageRoute(builder: (context) => Login()));
-              },
-              child: Text(
-                'Logout', //submit
-                style: TextStyle(
-                  fontSize: 20,
-                  fontFamily: "OpenSans Regular",
-                ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+            child: Container(
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Icon(Icons.person),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      "Account",
+                      style: TextStyle(
+                          fontFamily: "OpenSans Regular", fontSize: 20),
+                    ),
+                  ),
+                  Spacer(),
+                  Icon(Icons.arrow_right),
+                ],
               ),
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Icon(Icons.help),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      "About",
+                      style: TextStyle(
+                          fontFamily: "OpenSans Regular", fontSize: 20),
+                    ),
+                  ),
+                  Spacer(),
+                  Icon(Icons.arrow_right),
+                ],
+              ),
+            ),
+          ),
+          // Align(
+          //   alignment: FractionalOffset.bottomCenter,
+          //   child: Center(
+          //     child: RaisedButton(
+          //       shape: RoundedRectangleBorder(
+          //         borderRadius: BorderRadius.circular(30),
+          //       ),
+          //       color: Colors.blueGrey[900],
+          //       textColor: Colors.white,
+          //       padding: EdgeInsets.symmetric(vertical: 8, horizontal: 120),
+          //       onPressed: () {
+          //         signOut();
+          //         Navigator.pushReplacement(context,
+          //             MaterialPageRoute(builder: (context) => Login()));
+          //       },
+          //       child: Text(
+          //         'Logout', //submit
+          //         style: TextStyle(
+          //           fontSize: 20,
+          //           fontFamily: "OpenSans Regular",
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ),
         ],
       ),
     );
