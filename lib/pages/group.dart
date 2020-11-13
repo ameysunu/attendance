4088ae02126d9fcdcfdca176c43c6eda444e7904
Amey Sunu <ameysunu00719@gmail.com>
2020-11-13 12:57:46 +0300

    ui fixes

--- lib/pages/group.dart
+++ lib/pages/group.dart
@@ -35,174 +35,181 @@ class _GroupState extends State<Group> {
         child: ListView(
           children: [
             Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 SizedBox(
                   height: 30,
                   width: 30,
                 ),
-                Row(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.05,
-                      child: Center(child: Text("1")),
-                    ),
-                    Container(
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              width: MediaQuery.of(context).size.width * 0.9,
-                              child: Card(
-                                color: Colors.blueGrey[900],
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.all(10.0),
-                                      child: Text(
-                                        widget.name,
-                                        style: TextStyle(
-                                            color: Colors.white, fontSize: 30),
-                                      ),
-                                    ),
-                                    Container(
-                                      child: Column(
-                                        children: [
-                                          Text(
-                                            "Attendance",
-                                            style: TextStyle(
-                                              fontFamily: 'OpenSans Regular',
+                Center(
+                  child: Row(
+                    children: [
+                      // Container(
+                      //   width: MediaQuery.of(context).size.width * 0.05,
+                      //   child: Center(child: Text("1")),
+                      // ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                width: MediaQuery.of(context).size.width * 0.95,
+                                child: Card(
+                                  color: Colors.blueGrey[900],
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(10.0),
+                                        child: Text(
+                                          widget.name,
+                                          style: TextStyle(
                                               color: Colors.white,
-                                              fontSize: 12,
-                                            ),
-                                          ),
-                                          Text(
-                                            "13%",
-                                            style: TextStyle(
+                                              fontSize: 30),
+                                        ),
+                                      ),
+                                      Container(
+                                        child: Column(
+                                          children: [
+                                            Text(
+                                              "Attendance",
+                                              style: TextStyle(
                                                 fontFamily: 'OpenSans Regular',
                                                 color: Colors.white,
-                                                fontSize: 18),
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                    Container(
-                                      child: Column(
-                                        children: [
-                                          Text(
-                                            "Uniform",
-                                            style: TextStyle(
-                                              fontFamily: 'OpenSans Regular',
-                                              color: Colors.white,
-                                              fontSize: 12,
+                                                fontSize: 12,
+                                              ),
                                             ),
-                                          ),
-                                          Text(
-                                            "80%",
-                                            style: TextStyle(
+                                            Text(
+                                              "13%",
+                                              style: TextStyle(
+                                                  fontFamily:
+                                                      'OpenSans Regular',
+                                                  color: Colors.white,
+                                                  fontSize: 18),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                      Container(
+                                        child: Column(
+                                          children: [
+                                            Text(
+                                              "Uniform",
+                                              style: TextStyle(
                                                 fontFamily: 'OpenSans Regular',
                                                 color: Colors.white,
-                                                fontSize: 18),
-                                          )
-                                        ],
+                                                fontSize: 12,
+                                              ),
+                                            ),
+                                            Text(
+                                              "80%",
+                                              style: TextStyle(
+                                                  fontFamily:
+                                                      'OpenSans Regular',
+                                                  color: Colors.white,
+                                                  fontSize: 18),
+                                            )
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          Row(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: Container(
-                                  child: Column(
-                                    children: [
-                                      Text(
-                                        widget.date,
-                                        style: TextStyle(
-                                          fontFamily: 'OpenSans SemiBold',
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Container(
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          widget.date,
+                                          style: TextStyle(
+                                            fontFamily: 'OpenSans SemiBold',
+                                          ),
                                         ),
-                                      ),
-                                      Text(
-                                        widget.status,
-                                        style: TextStyle(
-                                            fontFamily: 'OpenSans SemiBold',
-                                            fontSize: 20),
-                                      ),
-                                      Text(
-                                        widget.total,
-                                        style: TextStyle(
-                                            fontFamily: 'OpenSans SemiBold',
-                                            fontSize: 25),
-                                      ),
-                                    ],
+                                        Text(
+                                          widget.status,
+                                          style: TextStyle(
+                                              fontFamily: 'OpenSans SemiBold',
+                                              fontSize: 20),
+                                        ),
+                                        Text(
+                                          widget.total,
+                                          style: TextStyle(
+                                              fontFamily: 'OpenSans SemiBold',
+                                              fontSize: 25),
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: Container(
-                                  child: Column(
-                                    children: [
-                                      Text(
-                                        widget.date,
-                                        style: TextStyle(
-                                          fontFamily: 'OpenSans SemiBold',
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Container(
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          widget.date,
+                                          style: TextStyle(
+                                            fontFamily: 'OpenSans SemiBold',
+                                          ),
                                         ),
-                                      ),
-                                      Text(
-                                        widget.status,
-                                        style: TextStyle(
-                                            fontFamily: 'OpenSans SemiBold',
-                                            fontSize: 20),
-                                      ),
-                                      Text(
-                                        widget.total,
-                                        style: TextStyle(
-                                            fontFamily: 'OpenSans SemiBold',
-                                            fontSize: 25),
-                                      ),
-                                    ],
+                                        Text(
+                                          widget.status,
+                                          style: TextStyle(
+                                              fontFamily: 'OpenSans SemiBold',
+                                              fontSize: 20),
+                                        ),
+                                        Text(
+                                          widget.total,
+                                          style: TextStyle(
+                                              fontFamily: 'OpenSans SemiBold',
+                                              fontSize: 25),
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: Container(
-                                  child: Column(
-                                    children: [
-                                      Text(
-                                        widget.date,
-                                        style: TextStyle(
-                                          fontFamily: 'OpenSans SemiBold',
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Container(
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          widget.date,
+                                          style: TextStyle(
+                                            fontFamily: 'OpenSans SemiBold',
+                                          ),
                                         ),
-                                      ),
-                                      Text(
-                                        widget.status,
-                                        style: TextStyle(
-                                            fontFamily: 'OpenSans SemiBold',
-                                            fontSize: 20),
-                                      ),
-                                      Text(
-                                        widget.total,
-                                        style: TextStyle(
-                                            fontFamily: 'OpenSans SemiBold',
-                                            fontSize: 25),
-                                      ),
-                                    ],
+                                        Text(
+                                          widget.status,
+                                          style: TextStyle(
+                                              fontFamily: 'OpenSans SemiBold',
+                                              fontSize: 20),
+                                        ),
+                                        Text(
+                                          widget.total,
+                                          style: TextStyle(
+                                              fontFamily: 'OpenSans SemiBold',
+                                              fontSize: 25),
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
-                          )
-                        ],
+                              ],
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ],
             ),
